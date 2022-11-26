c1ea02846d83c3bd3a0fcb226ed57a8ae92dab3d
Vishal Donga <vdonga@atlassian.com>
2022-11-26 10:10:39 +0530

    first commit

---
+++ js/variable.js
@@ -0,0 +1,53 @@
+console.log(2 + 3)
+/*
+ubad
+dsd
+sdsd
+*/
+//Shortcut => Ctrl K + Ctrl+C => Comment
+//Ctrl+K _ Ctrl +U => Uncomment
+
+//JS is not Object oriented language
+
+//Variables (Remember)
+//1. let
+//2. const
+//3. var
+
+//Datatypes(Important -> Remember)
+//1. String => "vishal" -> with quotes
+console.log("vishal") //datatype-> string
+//2. number
+console.log(2) // datatype => number
+//3. Boolean //true  and false
+console.log(true) //datatype-> boolean
+//4. Object
+//1. Array => []
+//==> We can iterate => loop array
+//2. Object => {}
+//==> key: value
+//Ex.{
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//   },
+//3. Function => ()
+
+//5. Null
+//let xyz = null;
+//6. Undefined
+//let xyz;
+//7. Symbol(Not important)
+//Use: Diffrentiate same values
+const symbol1 = Symbol(5);
+const symbol2 = Symbol(5);
+console.log(symbol1 === symbol2)
+
+const val1 = 5;
+const val2 = 5;
+console.log(val1 === val2)
+//How to check datattypes
+//typeof operator
+console.log(typeof "vsiahl")
+
+//Falsy Values
